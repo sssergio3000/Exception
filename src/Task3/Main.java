7124e97a41af5f63b959771b4edc91dbdbca5088
Sergey Khaykov <ssserg3000@gmail.com>
2022-12-10 17:47:22 +0200

    update1

--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -1,19 +1,42 @@
 package Task3;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Price[] products = new Price[2];
         String productName;
         String shopName;
         int price;
         int count = 0;
         Scanner scText = new Scanner(System.in);
         Scanner scInt = new Scanner(System.in);
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < products.length; i++) {
             System.out.println("Enter product name: ");
-            
+            productName = scText.nextLine();
+            System.out.println("Enter shop name: ");
+            shopName = scText.nextLine();
+            System.out.println("Enter price: ");
+            price = scInt.nextInt();
+            products[i] = new Price(productName, shopName, price);
+        }
+        System.out.println();
+        System.out.println();
+        Arrays.sort(products, Comparator.comparing(Price::getShopName));
+        System.out.println("Enter shop name: ");
+        shopName = scText.nextLine();
+
+        for (int i = 0; i < products.length; i++) {
+            if (products[i].getShopName().equals(shopName) ){
+                count++;
+                System.out.println(products[i]);
+            }
+        }
+        if (count==0){
+            throw new Exception ("No such shop!");
         }
 
     }
